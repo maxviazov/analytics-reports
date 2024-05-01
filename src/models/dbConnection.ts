dadcc4984f06bf220ad50f1aa3f9779ba916817f
LevKoganov <levk@noviopus.com>
2024-05-01 18:54:14 +0300

    database connection

---
+++ src/models/dbConnection.ts
@@ -0,0 +1,12 @@
+import { Client } from "pg";
+import { dbConfig } from "./constants";
+export const client = new Client(dbConfig);
+
+export default async () => {
+  try {
+    await client.connect();
+    console.log("Connected to PostgreSQL database");
+  } catch (err) {
+    console.error("Error connecting to PostgreSQL database", err);
+  }
+};
